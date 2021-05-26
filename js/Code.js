cce7774a2b32f15876ed041b50bfeb7edb70e42f
Prisca201 <83772158+Prisca201@users.noreply.github.com>
2021-05-26 18:40:43 +0530

    Add files via upload

---
+++ js/Code.js
@@ -0,0 +1,26 @@
+const accessCode1 = "VARIABLE";
+const accessCode2 = "FUNCTION";
+const accessCode3 = "DATABASE";
+
+function clues() {
+    
+    fill("white")
+    textSize(15)
+    text("R E V B A I L A", 100,50)
+    fill("lightblue")
+    text("Hint: Always changing, not constant !!", 100,70)
+   
+    fill("white")
+    textSize(15)
+    text("ATADASABA", 100,180);
+    fill("lightblue")
+    text("Hint: A systematic collection of data!!", 100,200)
+
+    fill("white")
+    textSize(15)
+    text("CUTNIFON", 700,240)
+    fill("lightblue")
+    text("Hint: Helps to execute a code!", 100,70);
+
+
+}
